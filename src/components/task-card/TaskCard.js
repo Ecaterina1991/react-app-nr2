4032ab087c5e4440c33ebcd2276f040bf5e35fdf
Ecaterina <barganecaterina@yahoo.com>
2022-10-13 23:51:28 +0300

    styling one component 1

--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -1,7 +1,19 @@
 import "./TaskCard.css";
 
 function TaskCard() {
-  return <div className="card-wrapper">Some text</div>;
+  return (
+    <div className="card-wrapper">
+      <div className="card-header">
+        <p className="task-id">T-1</p>
+
+        <div className="badge">
+          <p>Todo</p>
+        </div>
+      </div>
+      <div className="card-content"></div>
+      <div className="card-footer"></div>
+    </div>
+  );
 }
 
 export default TaskCard;
