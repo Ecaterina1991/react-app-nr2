8983cd5bdcefcc2a9b72716f3f4ffbe994579b35
Ecaterina <barganecaterina@yahoo.com>
2022-11-03 11:37:42 +0200

    object state

--- src/components/forms/CreateTaskForm.jsx
+++ src/components/forms/CreateTaskForm.jsx
@@ -1,23 +1,90 @@
 import React from "react";
+import { useState } from "react";
 import "./CreateTaskForm.css";
 
 const CreateTaskForm = () => {
+  // const [taskName, setTaskName] = useState("");
+  // const [dueDate, setDueDate] = useState("");
+  // const [taskDetails, setTaskDetails] = useState("");
+
+  const [formData, setFormData] = useState({
+    taskName: "",
+    dueDate: "",
+    taskDetails: "",
+  });
+  /*
+  const handleNameChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      taskName: event.target.value,
+    }));
+    //setTaskName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      dueDate: event.target.value,
+    }));
+    //setDueDate(event.target.value);
+  };
+
+  const handleDetailsChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      taskDetails: event.target.value,
+    }));
+    //setTaskDetails(event.target.value);
+  };*/
+
+  const handleInputChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("formData", formData);
+
+    // const newTask = {
+    //   name: taskName,
+    //   dueDate: dueDate,
+    //   taskDetails: taskDetails,
+    //   status: "To do",
+    // };
+    // console.log("newTask = ", newTask);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-row">
           <label className="label-md">Task Name</label>
-          <input className="input-primary" type="text" />
+          <input
+            name="taskName"
+            onChange={handleInputChange}
+            className="input-primary"
+            type="text"
+          />
         </div>
 
         <div className="form-row">
           <label className="label-md">Due Date</label>
-          <input className="case" type="date" />
+          <input
+            name="dueDate"
+            onChange={handleInputChange}
+            className="input-primary"
+            type="date"
+          />
         </div>
 
         <div className="form-row">
           <label className="label-md">Task Details</label>
           <textarea
+            name="taskDetails"
+            onChange={handleInputChange}
             className="input-primary"
             name=""
             id=""
