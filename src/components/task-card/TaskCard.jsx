ed6a1be741f53ef33c5b93d6b368fea5493a91c1
Ecaterina <barganecaterina@yahoo.com>
2022-11-15 15:41:35 +0200

    react developer tools

--- src/components/task-card/TaskCard.jsx
+++ src/components/task-card/TaskCard.jsx
@@ -1,21 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 import "./TaskCard.css";
 import Badge from "../badge/Badge";
 import DateContainer from "../date-container/DateContainer";
 
 function TaskCard(props) {
-  const [taskId, setTaskId] = useState(props.id);
+  // const [taskId, setTaskId] = useState(props.id);
 
-  const handleClick = () => {
-    setTaskId("Clicked!");
-    console.log(taskId);
+  // const handleClick = () => {
+  //   setTaskId("Clicked!");
+  //   console.log(taskId);
+  // };
+
+  const getBadgeStyle = () => {
+    switch (props.status) {
+      case "Todo":
+        return "grey";
+      case "In Progress":
+        return "blue";
+      case "Completed":
+        return "green";
+      case "Pending":
+        return "orange";
+      default:
+        return "grey";
+    }
   };
 
   return (
     <div className="card-wrapper">
       <div className="card-header">
-        <p className="task-id">{taskId}</p>
-        <Badge status={props.status} />
+        <p className="task-id">{props.id}</p>
+        {/* <Badge status={props.status} /> */}
+        <Badge label={props.status} color={getBadgeStyle()} />
       </div>
       <div className="card-content">
         <p>{props.name}</p>
